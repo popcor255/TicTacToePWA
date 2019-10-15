e4828d12b4976dc8ddb39f4775d3036be2b6dd26
popcor255 <popcor255@gmail.com>
2019-10-15 00:08:13 -0400

    basic game

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -50,7 +50,7 @@ export class BoardComponent implements OnInit {
 
     for(let i = 0; i < lines.length; i++){
       const [a, b, c] = lines[i];
-      if(this.squares[a] && this.squares[a] === this.squares[b] && this.squares == this.squares[c]){
+      if(this.squares[a] && this.squares[a] === this.squares[b] && this.squares[a] === this.squares[c]){
         return this.squares[a];
       }
     }
--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -3,18 +3,12 @@ import { Component, Input} from '@angular/core';
 @Component({
   selector: 'app-square',
   template: `
-    <p>
-      {{ rando }}
-    </p>
+    <button>{{value}}</button>
   `,
-  styles: []
+  styles: ['button { height: 100%; width: 100%}']
 })
 
 export class SquareComponent{
 
   @Input() value: 'X' | 'O';
-
-  constructor(){
-    setInterval(() => this.rando = Math.random(), 500);
-  }
 }
