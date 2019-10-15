a313ce37257146bea00d50cf06c64be178387482
popcor255 <popcor255@gmail.com>
2019-10-14 23:34:31 -0400

    new class

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,20 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  template: `
+    <p>
+      {{ rando }}
+    </p>
+  `,
+  styles: []
+})
+
+export class SquareComponent{
+
+  rando = Math.random();
+
+  constructor(){
+    setInterval(() => this.rando = Math.random(), 500);
+  }
+}
